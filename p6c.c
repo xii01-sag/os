14567a86811000d44c6389c91269f10f32067690
xii01 <rahulsagarxii01@gmail.com>
2025-12-31 09:53:56 +0530

    Create p6c.c

---
+++ p6c.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define MAX 25
+
+int main() {
+    int frag[MAX], b[MAX], f[MAX];
+    int bf[MAX] = {0}, ff[MAX] = {0};
+    int nb, nf, temp;
+    int i, j;
+
+    printf("\n\tMemory Management Scheme - First Fit");
+    printf("\nEnter the number of blocks: ");
+    scanf("%d", &nb);
+    printf("Enter the number of files: ");
+    scanf("%d", &nf);
+
+    printf("\nEnter the size of the blocks:\n");
+    for (i = 1; i <= nb; i++) {
+        printf("Block %d: ", i);
+        scanf("%d", &b[i]);
+    }
+
+    printf("\nEnter the size of the files:\n");
+    for (i = 1; i <= nf; i++) {
+        printf("File %d: ", i);
+        scanf("%d", &f[i]);
+    }
+
+    for (i = 1; i <= nf; i++) {
+        ff[i] = -1;
+        for (j = 1; j <= nb; j++) {
+            if (bf[j] == 0) {
+                temp = b[j] - f[i];
+                if (temp >= 0) {
+                    ff[i] = j;
+                    frag[i] = temp;
+                    bf[j] = 1;
+                    break; // allocate first suitable block
+                }
+            }
+        }
+        if (ff[i] == -1) frag[i] = -1;
+    }
+
+    printf("\nFile_no\tFile_size\tBlock_no\tBlock_size\tFragment");
+    for (i = 1; i <= nf; i++) {
+        if (ff[i] != -1)
+            printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",
+                   i, f[i], ff[i], b[ff[i]], frag[i]);
+        else
+            printf("\n%d\t\t%d\t\tNot Allocated\t-\t\t-", i, f[i]);
+    }
+
+    return 0;
+}
