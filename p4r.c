6c217f533929a7367bbbfc974b45a1c9bf47f65e
xii01 <rahulsagarxii01@gmail.com>
2025-12-31 09:51:56 +0530

    Create p4r.c

---
+++ p4r.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define MAX_BUF 1024
+
+int main() {
+    int fd;
+    char *myfifo = "/tmp/myfifo";
+    char buf[MAX_BUF];
+    fd = open(myfifo, O_RDONLY);
+    read(fd, buf, MAX_BUF);
+    printf("Writer: %s\n", buf);
+    close(fd);
+    return 0;
+}
